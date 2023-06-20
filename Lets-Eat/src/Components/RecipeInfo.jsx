1f6710adb19eff55065f4c1c6a84fa64071da6e2
TianaD <tldavis99@yahoo.com>
2023-06-20 11:37:43 -0500

    Added More Spaces for Ingredients | Removed Colon so if no Item is Present, Nothing Will Display

--- Lets-Eat/src/Components/RecipeInfo.jsx
+++ Lets-Eat/src/Components/RecipeInfo.jsx
@@ -52,14 +52,25 @@ const RecipeInfo = () => {
                 
                 <div className="ingredients">
                     <h2>Ingredients</h2>
-                    <h4>{item.strIngredient1}: {item.strMeasure1}</h4>
-                    <h4>{item.strIngredient2}: {item.strMeasure2}</h4>
-                    <h4>{item.strIngredient3}: {item.strMeasure3}</h4>
-                    <h4>{item.strIngredient4}: {item.strMeasure4}</h4>
-                    <h4>{item.strIngredient5}: {item.strMeasure5}</h4>
-                    <h4>{item.strIngredient6}: {item.strMeasure6}</h4>
-                    <h4>{item.strIngredient7}: {item.strMeasure7}</h4>
-                    <h4>{item.strIngredient8}: {item.strMeasure8}</h4>
+                    <h4>{item.strIngredient1} {item.strMeasure1}</h4>
+                    <h4>{item.strIngredient2} {item.strMeasure2}</h4>
+                    <h4>{item.strIngredient3} {item.strMeasure3}</h4>
+                    <h4>{item.strIngredient4} {item.strMeasure4}</h4>
+                    <h4>{item.strIngredient5} {item.strMeasure5}</h4>
+                    <h4>{item.strIngredient6} {item.strMeasure6}</h4>
+                    <h4>{item.strIngredient7} {item.strMeasure7}</h4>
+                    <h4>{item.strIngredient8} {item.strMeasure8}</h4>
+                    <h4>{item.strIngredient9} {item.strMeasure9}</h4>
+                    <h4>{item.strIngredient10} {item.strMeasure10}</h4>
+                    <h4>{item.strIngredient11} {item.strMeasure11}</h4>
+                    <h4>{item.strIngredient12} {item.strMeasure12}</h4>
+                    <h4>{item.strIngredient13} {item.strMeasure13}</h4>
+                    <h4>{item.strIngredient14} {item.strMeasure14}</h4>
+                    <h4>{item.strIngredient15} {item.strMeasure15}</h4>
+                    <h4>{item.strIngredient16} {item.strMeasure16}</h4>
+                    <h4>{item.strIngredient17} {item.strMeasure17}</h4>
+                    <h4>{item.strIngredient18} {item.strMeasure18}</h4>
+                    <h4>{item.strIngredient19} {item.strMeasure19}</h4>
                 </div>
 
                 <div className="Instructions">
